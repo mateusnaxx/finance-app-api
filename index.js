3f8cea66f5e44aa6a77e9f44f463714bf7c554f0
Mateus <mateus.naxx@gmail.com>
2025-07-30 11:01:53 -0300

    chore: add pg and dotenv setup

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const name = 'mateus'
+import 'dotenv/config.js'
+import express from 'express'
 
-console.log(name)
+import { postgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+  const results = await postgresHelper.query('SELECT * FROM users;')
+
+  res.send(JSON.stringify(results))
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
