0753cf29dd18dc108653506ea645d1b691bc81c2
mateusnaxx <mateus.naxx@gmail.com>
2025-08-08 15:16:32 -0300

    feat: add PostgresUpdateUserRepository

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,27 @@
+import { PostgresHelper } from '../../db/postgres/helper.js'
+
+export class PostgresUpdateUserRepository {
+    async execute(userId, updateUserParams) {
+        const updateFields = [] //[first_name = $1, last_name = $2]
+        const updateValues = [] // [Mateus, Almeida]
+
+        Object.keys(updateUserParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateFields.length + 1}`)
+            updateValues.push(updateUserParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+            UPDATE users
+            SET ${updateFields.join(', ')}
+            WHERE id = $${updateFields.length + 1}
+            RETURNING *;
+        `
+
+        // 'first_name = $1, last_name = $2'
+
+        const updateUser = await PostgresHelper.query(updateQuery, updateValues)
+        return updateUser[0]
+    }
+}
