6347f0c93a00f1d32e7584c982ae3155b9f71829
mateusnaxx <mateus.naxx@gmail.com>
2025-08-10 22:23:02 -0300

    feat: add dependency injection to delete user route

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ import { PostgresGetUserByEmailRepository } from './src/repositories/postgres/ge
 import { CreateUserUseCase } from './src/use-cases/create-user.js'
 import { PostgresUpdateUserRepository } from './src/repositories/postgres/update-user.js'
 import { UpdateUserUseCase } from './src/use-cases/update-user.js'
+import { PostgresDeleteUserRepository } from './src/repositories/postgres/delete-user.js'
+import { DeleteUserUseCase } from './src/use-cases/delete-user.js'
 
 const app = express()
 
@@ -64,7 +66,11 @@ app.patch('/api/users/:userId', async (req, res) => {
 })
 
 app.delete('/api/users/:userId', async (req, res) => {
-    const deleteUserController = new DeleteUserController()
+    const deleteUserRepository = new PostgresDeleteUserRepository()
+
+    const deleteUserUseCase = new DeleteUserUseCase(deleteUserRepository)
+
+    const deleteUserController = new DeleteUserController(deleteUserUseCase)
 
     const { statusCode, body } = await deleteUserController.execute(req)
 
--- src/use-cases/delete-user.js
+++ src/use-cases/delete-user.js
@@ -1,9 +1,10 @@
-import { PostgresDeleteUserRepository } from '../repositories/postgres/index.js'
-
 export class DeleteUserUseCase {
+    constructor(deleteUserRepository) {
+        this.deleteUserRepository = deleteUserRepository
+    }
+
     async execute(userId) {
-        const postgresDeleteUserRepository = new PostgresDeleteUserRepository()
-        const deletedUser = postgresDeleteUserRepository.execute(userId)
+        const deletedUser = await this.deleteUserRepository.execute(userId)
         return deletedUser
     }
 }
