fb1c187ed3282644f698964bbf9a28c780a1bf40
mateusnaxx <mateus.naxx@gmail.com>
2025-08-11 14:54:11 -0300

    feat: add CreateTransactionUseCase

--- src/errors/user.js
+++ src/errors/user.js
@@ -4,3 +4,10 @@ export class EmailAlreadyInUseError extends Error {
         this.name = 'EmailAlreadyInUseError'
     }
 }
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`User not found: ${userId}`)
+        this.name = 'UserNotFoundError'
+    }
+}
