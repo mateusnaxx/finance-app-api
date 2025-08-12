62ccefd5517afa03161f3ad4930a47c529ca3e3e
mateusnaxx <mateus.naxx@gmail.com>
2025-08-12 20:59:02 -0300

    feat: PostgresUpdateTransactionRepository

---
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -0,0 +1,25 @@
+import { postgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresUpdateTransactionRepository {
+    async execute(userId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateFields.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(userId)
+
+        const updateQuery = `
+                UPDATE transactions
+                SET ${updateFields.join(', ')}
+                WHERE id = $${updateFields.length + 1}
+                RETURNING *;
+            `
+
+        const updateUser = await postgresHelper.query(updateQuery, updateValues)
+        return updateUser[0]
+    }
+}
