cbb32210501d3c715d7dae4e7786a8f6ecb55819
mateusnaxx <mateus.naxx@gmail.com>
2025-08-12 17:25:53 -0300

    feat: add route to get transactions by user id

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import {
     makeUpdateUserController,
 } from './src/factories/controllers/user.js'
 
-import { makeCreateTransactionController } from './src/factories/controllers/transaction.js'
+import {
+    makeCreateTransactionController,
+    makeGetTransactionsByUserIdController,
+} from './src/factories/controllers/transaction.js'
 
 const app = express()
 
@@ -40,6 +43,15 @@ app.delete('/api/users/:userId', async (req, res) => {
     const deleteUserController = makeDeleteUserController()
 
     const { statusCode, body } = await deleteUserController.execute(req)
+
+    res.status(statusCode).send(body)
+})
+
+app.get('/api/transactions', async (req, res) => {
+    const getTransactionsByUserIdController =
+        makeGetTransactionsByUserIdController()
+    const { statusCode, body } =
+        await getTransactionsByUserIdController.execute(req)
 
     res.status(statusCode).send(body)
 })
