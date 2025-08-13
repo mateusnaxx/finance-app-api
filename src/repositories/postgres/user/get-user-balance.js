bcf3cc7540b963a8f46cdd949d5b49e575b34936
mateusnaxx <mateus.naxx@gmail.com>
2025-08-13 10:28:38 -0300

    feat: add PostgresGetUserBalanceRepository

---
+++ src/repositories/postgres/user/get-user-balance.js
@@ -0,0 +1,25 @@
+import { postgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await postgresHelper.query(
+            `SELECT 
+                SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) AS earnings,
+                SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END) AS expenses,
+                SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END) AS investments,
+                (
+                    SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
+                    - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+                    - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
+                ) AS balance
+            FROM transactions
+            WHERE user_id = $1`,
+            [userId],
+        )
+
+        return {
+            userId,
+            ...balance[0],
+        }
+    }
+}
