45a59ac1d9b0861743ac915b4712c45093e58e3a
mateusnaxx <mateus.naxx@gmail.com>
2025-08-13 10:58:39 -0300

    feat: add route to get user balance

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import {
     makeCreateUserController,
     makeDeleteUserController,
+    makeGetUserBalanceController,
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './src/factories/controllers/user.js'
@@ -21,6 +22,13 @@ app.use(express.json())
 app.get('/api/users/:userId', async (req, res) => {
     const getUserByIdController = makeGetUserByIdController()
     const { statusCode, body } = await getUserByIdController.execute(req)
+
+    res.status(statusCode).send(body)
+})
+
+app.get('/api/users/:userId/balance', async (req, res) => {
+    const getUserBalanceController = makeGetUserBalanceController()
+    const { statusCode, body } = await getUserBalanceController.execute(req)
 
     res.status(statusCode).send(body)
 })
