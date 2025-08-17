4df55ff977045fb006794c743f22b769ecbe8277
mateusnaxx <mateus.naxx@gmail.com>
2025-08-17 18:40:46 -0300

    feat: unsure GetUserBalanceRepository use Prisma

--- src/repositories/postgres/user/get-user-balance.js
+++ src/repositories/postgres/user/get-user-balance.js
@@ -1,16 +1,57 @@
-import { postgresHelper } from '../../../db/postgres/helper.js'
+import { Prisma } from '@prisma/client'
+import prisma from '../../../../prisma/prisma.js'
 
 export class PostgresGetUserBalanceRepository {
     async execute(userId) {
-        const balance = await postgresHelper.query(
-            `SELECT earnings, expenses, investments, balance FROM view_user_balance
-            WHERE user_id = $1`,
-            [userId],
+        const {
+            _sum: { amount: totalEarnings },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EARNING',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalExpenses },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'EXPENSE',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const {
+            _sum: { amount: totalInvestments },
+        } = await prisma.transaction.aggregate({
+            where: {
+                user_id: userId,
+                type: 'INVESTMENT',
+            },
+            _sum: {
+                amount: true,
+            },
+        })
+
+        const _totalEarnings = totalEarnings || new Prisma.Decimal(0)
+        const _totalExpenses = totalExpenses || new Prisma.Decimal(0)
+        const _totalInvestments = totalInvestments || new Prisma.Decimal(0)
+
+        const balance = new Prisma.Decimal(
+            _totalEarnings - _totalExpenses - _totalInvestments,
         )
 
         return {
-            userId,
-            ...balance[0],
+            totalEarnings: _totalEarnings,
+            totalExpenses: _totalExpenses,
+            totalInvestments: _totalInvestments,
+            balance,
         }
     }
 }
